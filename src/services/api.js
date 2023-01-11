5994dee5049fc138f3ddf9b821da6e1b4356a98e
natalia <ntla.brs@gmail.com>
2023-01-11 16:19:50 -0300

    implementando funcões getCat e getProduct

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,32 @@
 export async function getCategories() {
-  
+  const URL = 'https://api.mercadolibre.com/sites/MLB/categories';
+  try {
+    const response = await fetch(URL);
+    const categories = response.json();
+    return categories;
+  } catch (e) {
+    console.log(e.message);
+  }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(CATEGORY_ID, QUERY) {
+  const URL_CAT = `https://api.mercadolibre.com/sites/MLB/search?category=${CATEGORY_ID}`;
+  const URL_QUERY = `https://api.mercadolibre.com/sites/MLB/search?category=${QUERY}`;
+  try {
+    const request = await fetch(URL_CAT);
+    const response = request.json();
+    return response;
+  } catch (e) {
+    console.log(e.message);
+  }
+
+  try {
+    const request = await fetch(URL_QUERY);
+    const response = request.json();
+    return response;
+  } catch (e) {
+    console.log(e.message);
+  }
 }
 
 export async function getProductById() {
